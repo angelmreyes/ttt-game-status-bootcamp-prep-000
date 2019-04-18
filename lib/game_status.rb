d46f987ee78ad7710c21bd20c8884f3aae447e49
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 18:54:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,6 +29,15 @@ end
 def full?(board)
   !board.any? { |position| position.nil? || position == "" || position == " " }
 end
-
+# Build a method `#draw?` that accepts a board and returns true if the board has
+# not been won but is full, false if the board is not won and the board is not
+# full, and false if the board is won
 def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  elsif won?(board)
+    return false
+  end
 end
