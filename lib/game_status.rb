a0dd99ee1746c916d2c27279076fb400c540498f
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 18:34:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination # return the win_combination indexes that won.
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      return win_combination
     else
       false
     end
